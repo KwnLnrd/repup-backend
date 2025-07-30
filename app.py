7d19022bbfb65b5e4fb41d48868c2ac2585e5b92
Kewan Leonard <mgt@Kwn-Lnrd.local>
2025-07-30 10:38:21 +0200

    correctifapp

--- app.py
+++ app.py
@@ -575,4 +575,4 @@ def trigger_strategic_analysis():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=Tr
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
